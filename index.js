1e1ee4bd2f322a165033c9f95bcc668783b23029
Kris Thompson <kt.devcat@gmail.com>
2018-01-21 20:28:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').getElementsByClassName('target')[0];
+}
+
+function increaseRankBy(n){
+  let lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+
+  for (let i = 0; i < lis.length; i++){
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  return document.getElementById('grand-node').querySelector('div div div div div');
+}
